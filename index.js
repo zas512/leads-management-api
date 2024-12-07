d9c706010539d741eb1fac56bc78e444804943ec
Zain Ali <zainalis.914@gmail.com>
2024-12-07 20:14:56 +0500

    deployment fixes

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ app.use(express.json());
 app.use("/users", usersRouter);
 app.use("/leads", leadsRouter);
 
-// app.use("/", (req, res) => {
-//   res.status(200).json({ message: "Hello World" });
-// });
+app.use("/", (req, res) => {
+  res.status(200).json({ message: "Hello World" });
+});
 app.use((req, res) => {
   res.status(404).json({ message: "Route not found" });
 });
