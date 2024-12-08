cc92e9755099775a6ae993b7b75e5d11c04af01e
Zain Ali <zainalis.914@gmail.com>
2024-12-08 20:36:32 +0500

    jwt fix

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -69,7 +69,8 @@ export const loginUser = async (req, res) => {
     console.log("[loginUser] Setting cookie with authentication token");
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV !== "development",
+      secure: true,
+      sameSite: "none",
       maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
     });
 
